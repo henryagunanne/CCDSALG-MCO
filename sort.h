1d92cb930946224773ac0a1aa241280c0886f8fb
qinn1129 <qinpei_lu@dlsu.edu.ph>
2025-07-01 00:30:25 +0800

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,3 +1,12 @@
+#ifndef POLAR_H
+#define POLAR_H
+
+#include <math.h>
+#include "point.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 typedef struct {
     double r;
@@ -15,3 +24,5 @@ void heapSortWithPoints(Point pts[], Polar polarArr[], int n);
 
 
 double orientation(Point p, Point q, Point r);
+
+#endif
