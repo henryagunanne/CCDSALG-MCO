80be4c0f712685e0fc3954d9cd030736c5ac6371
chazinocencio <101245423+chazinocencio@users.noreply.github.com>
2025-06-30 20:34:36 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,24 +1,15 @@
-#include "point.h"
 
-#define MAX_POINT_COUNT (1 << 15)
-
+#define STACK_H
 typedef struct {
     Point items[MAX_POINT_COUNT];
     int topIndex;
 } Stack;
 
 void CREATE(Stack *S);
-
 int ISEMPTY(Stack *S);
-
 int ISFULL(Stack *S);
- 
 void PUSH(Stack *S, Point elem);
-
 Point POP(Stack *S);
-
 Point TOP(Stack *S);
-
 Point NEXT_TO_TOP(Stack *S);
-
 int GET_STACK(Stack *S, Point outArray[]);
