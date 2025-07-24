91ea23b862dcc6ead98fc208a61e65ea5946137c
henryagunanne <henryagunanne@gmail.com>
2025-07-24 22:20:41 +0800

    documentation

--- MCO2/graph.h
+++ MCO2/graph.h
@@ -5,40 +5,145 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * @def MAX_VERTICES
+ * Maximum number of vertices allowed in the graph.
+ */
 #define MAX_VERTICES 20
+
+/**
+ * @def MAX_NAME_LEN
+ * Maximum length for vertex names.
+ */
 #define MAX_NAME_LEN 9
+
+/**
+ * @def MAX_LINE_LEN
+ * Maximum length for a line in text files.
+ */
 #define MAX_LINE_LEN 256
 
+/**
+ * @typedef String8
+ * Type alias for a string of 8 characters (used for vertex names).
+ */
 typedef char String8[MAX_NAME_LEN];
 
+/**
+ * @struct Graph
+ * Represents a simple undirected graph using adjacency matrix.
+ */
 struct Graph {
-    int adjMatrix[MAX_VERTICES][MAX_VERTICES];
-    String8 names[MAX_VERTICES];
-    int vertexCount;
+    int adjMatrix[MAX_VERTICES][MAX_VERTICES];  /**< Adjacency matrix representing edges */
+    String8 names[MAX_VERTICES];                /**< Names of the vertices */
+    int vertexCount;                            /**< Number of vertices */
 };
 
+/**
+ * @struct Edge
+ * Represents an undirected edge between two vertices.
+ */
 struct Edge{
-    char u[50];
-    char v[50];
+    char u[50]; /**< First vertex name */
+    char v[50]; /**< Second vertex name */
 };
 
+/**
+ * @struct Line
+ * Stores a line from the input file (source and its neighbors).
+ */
 struct Line{
-    int neighborCount;
-    String8 src;
-    String8 neighbors[MAX_VERTICES];
+    int neighborCount;                   /**< Number of neighbors for this source */
+    String8 src;                         /**< Source vertex name */
+    String8 neighbors[MAX_VERTICES];     /**< Array of neighbor vertex names */
 };
 
+/**
+ * @brief Initializes and returns an empty graph.
+ * @return An empty Graph struct.
+ */
 struct Graph initGraph(); //DONE
-int getVertexIndex(struct Graph* g, String8 name, int* indexOut); //DONE
-void addEdge(struct Graph* g, String8 name1, String8 name2); //DONE
-int readGraphFromFile(struct Graph* g, String8 filename, struct Line lines[]); //DONE
-void set(struct Graph g, String8 filename); //DONE
-void list(struct Graph g, String8 filename, struct Line lines[], int lineCount); // !!! NEED EDIT
-void matrix(struct Graph g, String8 filename); //DONE
-void degree (struct Graph g, String8 filename); //DONE
-void BFS(struct Graph g, String8 startVertex,String8 filename); //DONE
-void DFS(struct Graph g, String8 startVertex,String8 filename); //DONE
-void bonus(struct Graph graph1, struct Graph graph2, String8 file1, String8 file2);
 
+/**
+ * @brief Returns the index of a vertex in graph g with name 'name'.
+ * Adds the vertex if not found (unless name is "-1").
+ * @param g Pointer to the graph.
+ * @param name Name of the vertex to find or add.
+ * @param indexOut Pointer to output index.
+ * @return 1 if found or added, 0 otherwise.
+ */
+int getVertexIndex(struct Graph* g, String8 name, int* indexOut); //DONE
+
+/**
+ * @brief Adds an undirected edge between name1 and name2 in graph g.
+ * @param g Pointer to the graph.
+ * @param name1 Name of the first vertex.
+ * @param name2 Name of the second vertex.
+ */
+void addEdge(struct Graph* g, String8 name1, String8 name2); //DONE
+
+/**
+ * @brief Reads a graph from the text file 'filename' and fills the lines[] array.
+ * @param g Pointer to the graph to fill.
+ * @param filename Name of the input file.
+ * @param lines Array of Line structs to fill with vertex/neighbor info.
+ * @return Number of lines (vertices) read, or 0 on error.
+ */
+int readGraphFromFile(struct Graph* g, String8 filename, struct Line lines[]); //DONE
+
+/**
+ * @brief Outputs the set representation of the graph to a file.
+ * @param g The graph to output.
+ * @param filename Name of the input file (used to construct output filename).
+ */
+void set(struct Graph g, String8 filename); //DONE
+
+/**
+ * @brief Outputs the adjacency list representation of the graph, preserving input order.
+ * @param g The graph to output.
+ * @param filename Name of the input file (used to construct output filename).
+ * @param lines Array of Line structs containing input order.
+ * @param lineCount Number of lines (vertices).
+ */
+void list(struct Graph g, String8 filename, struct Line lines[], int lineCount); // !!! NEED EDIT
+
+/**
+ * @brief Outputs the adjacency matrix representation of the graph to a file.
+ * @param g The graph to output.
+ * @param filename Name of the input file (used to construct output filename).
+ */
+void matrix(struct Graph g, String8 filename); //DONE
+
+/**
+ * @brief Outputs the degree of each vertex in the graph to a file.
+ * @param g The graph to output.
+ * @param filename Name of the input file (used to construct output filename).
+ */
+void degree (struct Graph g, String8 filename); //DONE
+
+/**
+ * @brief Performs BFS from the given start vertex and outputs traversal to a file.
+ * @param g The graph to traverse.
+ * @param startVertex Name of the starting vertex.
+ * @param filename Name of the input file (used to construct output filename).
+ */
+void BFS(struct Graph g, String8 startVertex,String8 filename); //DONE
+
+/**
+ * @brief Performs DFS from the given start vertex and outputs traversal to a file.
+ * @param g The graph to traverse.
+ * @param startVertex Name of the starting vertex.
+ * @param filename Name of the input file (used to construct output filename).
+ */
+void DFS(struct Graph g, String8 startVertex,String8 filename); //DONE
+
+/**
+ * @brief Checks if one graph is a subgraph of another and outputs result to a file.
+ * @param graph1 The main graph.
+ * @param graph2 The subgraph to check.
+ * @param file1 Name of the first input file.
+ * @param file2 Name of the second input file.
+ */
+void bonus(struct Graph graph1, struct Graph graph2, String8 file1, String8 file2);
 
 #endif // GRAPH_H
--- MCO2/main.c
+++ MCO2/main.c
@@ -1,48 +1,53 @@
-
 #include "graph.h"
 
+/**
+ * @file main.c
+ * @brief Main program for reading a graph file, outputting its representations, and performing traversals.
+ */
 
+/**
+ * @brief Main entry point for the program.
+ * Prompts for input file, reads graph, outputs representations, and performs BFS/DFS.
+ * @return 0 on success, non-zero on failure.
+ */
 int main() {
     struct Graph graph;
     int i, startIndex = -1;
     int lineCount;
-    struct Line lines[MAX_VERTICES];
-    
-    
+    struct Line lines[MAX_VERTICES]; /**< Stores input file lines for adjacency list output */
+
     String8 filename;
     String8 startVertex;
 
     printf("Enter input filename (e.g., G.TXT): ");
     scanf("%s", filename);
 
-    lineCount = readGraphFromFile(&graph, filename,lines);
-    if(lineCount){
-      set(graph,filename);
-      degree(graph, filename);
-      list(graph, filename,lines,lineCount);
-      matrix(graph,filename);
-      
-      printf("Enter input Start Vertex (e.g., Clark): ");
-      scanf("%s", startVertex);
-
-      // Find index of the starting vertex
-      for (i = 0; i < graph.vertexCount; i++) {
-        if (strcmp(graph.names[i], startVertex) == 0) {
-            startIndex = i;
-        }
-      }
-
-      if (startIndex == -1) {
-        printf("Vertex %s not found in the graph.\n", startVertex);
-      }else{
-        BFS(graph,startVertex,filename);
-        DFS(graph, startVertex, filename);
-      }
+    /* Read the graph from file and fill lines[] array */
+    lineCount = readGraphFromFile(&graph, filename, lines);
+    if (lineCount) {
+        set(graph, filename);               /**< Output set representation */
+        degree(graph, filename);            /**< Output vertex degrees */
+        list(graph, filename, lines, lineCount); /**< Output adjacency list (input order) */
+        matrix(graph, filename);            /**< Output adjacency matrix */
         
-    }else{
-       printf("File %s not found", filename);
+        printf("Enter input Start Vertex (e.g., Clark): ");
+        scanf("%s", startVertex);
+
+        /* Find index of the starting vertex (just for error reporting) */
+        for (i = 0; i < graph.vertexCount; i++) {
+            if (strcmp(graph.names[i], startVertex) == 0) {
+                startIndex = i;
+            }
+        }
+
+        if (startIndex == -1) {
+            printf("Vertex %s not found in the graph.\n", startVertex);
+        } else {
+            BFS(graph, startVertex, filename); /**< Perform BFS and output */
+            DFS(graph, startVertex, filename); /**< Perform DFS and output */
+        }
+    } else {
+        printf("File %s not found", filename);
     }
-
-  return 0;
+    return 0;
 }
-
